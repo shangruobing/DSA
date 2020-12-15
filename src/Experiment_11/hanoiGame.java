26df7616661e4de9800ce222e5251e00934661b0
shangruobing <shangruobing29@163.com>
2020-12-15 21:39:19 +0800

    Commit Message
    Signed-off-by: shangruobing <shangruobing29@163.com>

--- src/Experiment_11/hanoiGame.java
+++ src/Experiment_11/hanoiGame.java
@@ -11,22 +11,22 @@ public class hanoiGame extends Frame implements ActionListener{
 	}
 	public static int Disks;
 	Hanoi h;
-	Button start,move;
+	Button confirm,start;
 	Panel pan;
-	TextField tf1;
+	TextField field;
 	public hanoiGame(){
-		Label lb1=new Label("盘数");
+		Label label=new Label("盘数");
+		confirm = new Button("确定");
 		start = new Button("开始");
+		field = new TextField(3);
+		confirm.addActionListener(this);
 		start.addActionListener(this);
-		move = new Button("移动");
-		move.addActionListener(this);
-		tf1 = new TextField(3);
 
 		pan = new Panel();
-		pan.add(lb1);
-		pan.add(tf1);
+		pan.add(label);
+		pan.add(field);
+		pan.add(confirm);
 		pan.add(start);
-		pan.add(move);
 		this.add(pan,BorderLayout.SOUTH);
 		setSize(400,300);
 		setTitle("实验十一  自动汉诺塔游戏  尚若冰");
@@ -41,13 +41,13 @@ public class hanoiGame extends Frame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() instanceof Button){ //点击按钮命令
 			Button bt=(Button)e.getSource();
-			if(bt==start&& tf1.getText()!=null){ //开始命令
-				Disks =Integer.parseInt(tf1.getText());
+			if(bt==confirm&& field.getText()!=null){ //确定输入的盘数
+				Disks =Integer.parseInt(field.getText());
 				h= new Hanoi();
 				this.add(h, BorderLayout.CENTER);
 				setSize(400,301);
 			}
-			if(bt==move){
+			if(bt==start){ //开始运行
 				h.solve();
 			}
 		}
